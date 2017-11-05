4ca0de40edcd46405bc0353b104705ea400e8083
IanChen6 <30455192+IanChen6@users.noreply.github.com>
2017-11-06 00:49:26 +0800

    variable
    variable

--- arguments/cls_variable.py
+++ arguments/cls_variable.py
@@ -3,15 +3,15 @@ __author__ = 'IanChen'
 
 
 class Test(object):
-    num_of_instance = 0#类变量
+    num_of_instance = 0  # 类变量
 
     def __init__(self, name):
-        self.name = name#实例变量
+        self.name = name  # 实例变量
         Test.num_of_instance += 1
 
 
 if __name__ == '__main__':
-    print(Test.num_of_instance) # 0
+    print(Test.num_of_instance)  # 0
     t1 = Test('jack')
     print(Test.num_of_instance)  # 1
     t2 = Test('lucy')
